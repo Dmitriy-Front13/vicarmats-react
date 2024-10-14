7c574333528db9bdbfbd12d7f63cb8e80b3992d6
Dima <dimaklim04@gmail.com>
2024-10-14 20:23:59 +0300

    fix gift modal and add style to modal

--- src/components/modals/ModalTemplate.js
+++ src/components/modals/ModalTemplate.js
@@ -18,6 +18,7 @@ const ModalTemplate = ({ modalOpen, setModalOpen, title, isDescription, image, i
             <span className='modal__closeIcon'>
               <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><path d="M.3 9.7c.2.2.4.3.7.3.3 0 .5-.1.7-.3L5 6.4l3.3 3.3c.2.2.5.3.7.3.2 0 .5-.1.7-.3.4-.4.4-1 0-1.4L6.4 5l3.3-3.3c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L5 3.6 1.7.3C1.3-.1.7-.1.3.3c-.4.4-.4 1 0 1.4L3.6 5 .3 8.3c-.4.4-.4 1 0 1.4z" fill="#fff" fill-rule="nonzero"></path></svg>
             </span>
+            <span className="modal__closeLabel">Close</span>
           </button>
           {contentElement}
         </div>}>
@@ -41,8 +42,8 @@ const ModalTemplate = ({ modalOpen, setModalOpen, title, isDescription, image, i
         </ul>
       </div>
 
-      {isGift && <button className="shopNow-btn">
-        <a href="#price-constructor" className="shohNow-link">Shop now</a>
+      {isGift && <button className="shopNow-btn" onClick={setModalOpen}>
+        <a href="#price-constructor">Shop now</a>
       </button>}
     </Modal>
 
